d7d0adfcbefffa2ba8d07db336fb14b3159d03b1
angemissiba <as.missiba@b2i-solutions.com>
2023-06-25 01:24:17 +0200

    #v1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const  morgan = require("morgan");
 const  helmet = require("helmet");
 const  cors  =  require("cors");
 const bodyParser = require("body-parser");
+const functions = require('firebase-functions');
 dotenv.config();
 
 
@@ -78,3 +79,5 @@ app.use(`${apiV1}`,visitorroute);
 app.listen(process.env.PORT, ()=>{
   console.log("serveur demaré "+process.env.PORT);
 })
+
+// exports.api = functions.https.onRequest(app);
