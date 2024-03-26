f9813dad29464b11891846fb60930441f0fe8936
angemissiba <as.missiba@b2i-solutions.com>
2024-03-26 08:35:48 +0100

    #v1

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const  morgan = require("morgan");
 const  helmet = require("helmet");
 const  cors  =  require("cors");
 const bodyParser = require("body-parser");
-const functions = require('firebase-functions');
 dotenv.config();
 
 
